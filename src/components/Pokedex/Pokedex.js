1cca14265bafaa84673111b482ec1503dcd76ad0
thiagoassalin <thiagos.assalin@gmail.com>
2022-04-05 07:47:22 -0300

    segundo commit

--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -7,22 +7,16 @@ import BtnAvVo from "../BtnAvVo/BtnAvVo";
 export default function Pokedex() {
   const [id, setId] = useState(1);
   const [poke, setPoke] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   var i;
   useEffect(() => {
     async function loadPoke() {
       const response = await api.get(`/pokemon/${id}`);
       setPoke(response.data);
-      console.log(response)
-      console.log(poke)
-      
     }
     loadPoke();
-    
   }, [id]);
- 
-  
+
   function nextPoke() {
     if (poke.id < 898) {
       i = poke.id + 1;
@@ -45,25 +39,17 @@ export default function Pokedex() {
     let idP = id;
     setId(idP);
   }
-  
-  
-
-  
-  
-  
   return (
     <div className="poke">
       <Pesquisa handleId={handleId}></Pesquisa>
       <div className="poke-card">
-        
         <h1 className="nome-poke">{poke.name}</h1>
-        <p>{poke.id}</p>
-        <img alt={poke.name}
+        <p></p>
+        <img
+          alt={poke.name}
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png`}
         />
-        <div>
-           
-        </div>
+        <div></div>
       </div>
       <BtnAvVo nextPoke={nextPoke} previusPoke={previusPoke}></BtnAvVo>
     </div>
